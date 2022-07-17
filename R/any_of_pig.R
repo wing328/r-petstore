12cb285bbad413f9307d05e9c13af4f8a4e30183
William Cheng <wing328hk@gmail.com>
2022-07-17 22:34:13 +0800

    update

--- R/any_of_pig.R
+++ R/any_of_pig.R
@@ -36,11 +36,11 @@ AnyOfPig <- R6::R6Class(
       if (is.null(instance)) {
         # do nothing
       } else if (get(class(instance)[[1]], pos = -1)$classname ==  "BasquePig") {
-        self$actual_instance = instance
-        self$actual_type = "BasquePig"
+        self$actual_instance <- instance
+        self$actual_type <- "BasquePig"
       } else if (get(class(instance)[[1]], pos = -1)$classname ==  "DanishPig") {
-        self$actual_instance = instance
-        self$actual_type = "DanishPig"
+        self$actual_instance <- instance
+        self$actual_type <- "DanishPig"
       } else {
         stop(paste("Failed to initialize AnyOfPig with anyOf schemas BasquePig, DanishPig. Provided class name: ",
                    get(class(instance)[[1]], pos = -1)$classname))
